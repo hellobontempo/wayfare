c2647213d503ae44b00c083441eb87c7215f7a97
claire <clairebontempo@gmail.com>
2020-12-07 12:01:58 -0800

    tables created

--- db/migrate/20201207194043_create_trips.rb
+++ db/migrate/20201207194043_create_trips.rb
@@ -1,8 +1,11 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
-
-      t.timestamps null: false
+      t.string :destination
+      t.string :notes
+      t.date :start_date
+      t.date :end_date
+      t.integer :user_id
     end
   end
 end
