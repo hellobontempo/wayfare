2d5446532ddbde9df72a2f2762fea3cf4350dd98
claire <clairebontempo@gmail.com>
2020-12-10 09:22:39 -0800

    resort index/show page updated

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,12 +1,14 @@
 class ResortsController < ApplicationController
 
   get "/resorts" do
+    redirect_if_not_logged_in
     @resorts = Resort.order(:state)
     erb :"/resorts/index"
   end
 
 
   get "/resorts/:id" do
+    redirect_if_not_logged_in
     @resort = Resort.find_by(params[:id])
     erb :"/resorts/show"
   end
