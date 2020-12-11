c3cbfaf64f636a2cc66c5eca9b2949622ba1b2fa
claire <clairebontempo@gmail.com>
2020-12-10 20:23:39 -0800

    added trip select option to resort show page

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -9,8 +9,16 @@ class ResortsController < ApplicationController
 
   get "/resorts/:id" do
     redirect_if_not_logged_in
-    @resort = Resort.find_by(params[:id])
+    @resort = Resort.find_by_id(params[:id])
+    @user = current_user
     erb :"/resorts/show"
   end
 
+  patch "/resorts/:id" do
+    redirect_if_not_logged_in
+    @user = current_user
+    @resort = Resort.find_by_id(params[:id])
+    @resort.trip_ids = params[:trips]
+    redirect "/users/#{@user.id}"
+  end
 end
