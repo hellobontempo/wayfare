375c6502189804dc6e68eef8532aba883b4c8b75
claire <clairebontempo@gmail.com>
2020-12-11 15:08:43 -0800

    README updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# --- REQUIRED TO SET UP DATABASE --- 
 State.create(name: "Colorado", code: "CO")
 State.create(name: "Washington", code: "WA")
 State.create(name: "Utah", code: "UT")
@@ -19,21 +20,17 @@ Resort.create(name: "Whistler Blackcomb", city: "Whistler", state_id: 4, url: "h
 Resort.create(name: "Sun Valley", city: "Sun Valley", state_id: 6, url: "https://www.sunvalley.com/")
 Resort.create(name: "Snowbasin", city: "Huntsville", state_id: 3, url: "https://www.snowbasin.com/")
 
-User.create(name: "claire", email: "claire@me.com", password: "12345")
 
-Trip.create(name: "lake tahoe", notes: "staying at lake tahoe beach resort", start_date: "2020-12-13", end_date: "2020-12-18", user_id: 1)
-Trip.create(name: "utah", notes: "car camping", start_date: "2020-12-19", end_date: "2020-12-20", user_id: 1)
-Trip.create(name: "colorado", notes: "need lodging for 12/23 and 12/31-1/4, hot springs ...", start_date: "2020-12-23", end_date: "2020-12-31", user_id: 1)
+# --- OPTIONAL FAKER DATA --- #
 
+# 5.times do
+#     Trip.create(name: Faker::TvShows::TwinPeaks.unique.location, start_date: Faker::Date.between(from: '2020-12-01', to: '2020-12-15'), end_date: Faker::Date.between(from: '2020-12-15', to: '2020-12-31'), notes: Faker::Quote.jack_handey, user_id: Faker::Number.within(range: 2..5))
+# end
 
-5.times do
-    Trip.create(name: Faker::TvShows::TwinPeaks.unique.location, start_date: Faker::Date.between(from: '2020-12-01', to: '2020-12-15'), end_date: Faker::Date.between(from: '2020-12-15', to: '2020-12-31'), notes: Faker::Quote.jack_handey, user_id: Faker::Number.within(range: 2..5))
-end
+# 5.times do
+#     Trip.create(name: Faker::Games::SuperMario.unique.location, start_date: Faker::Date.between(from: '2020-12-01', to: '2020-12-15'), end_date: Faker::Date.between(from: '2020-12-15', to: '2020-12-31'), notes: Faker::Quote.jack_handey, user_id: Faker::Number.within(range: 2..5))
+# end
 
-5.times do
-    Trip.create(name: Faker::Games::SuperMario.unique.location, start_date: Faker::Date.between(from: '2020-12-01', to: '2020-12-15'), end_date: Faker::Date.between(from: '2020-12-15', to: '2020-12-31'), notes: Faker::Quote.jack_handey, user_id: Faker::Number.within(range: 2..5))
-end
-
-5.times do
-    User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, password: "123password")
-end
+# 5.times do
+#     User.create(name: Faker::Name.first_name , email: Faker::Internet.unique.email, password: "123password")
+# end
